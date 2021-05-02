c68d2ebcbafb73ec44260691ef089484dc16b3c2
Kushagra Srivastava <58633364+kushagras71@users.noreply.github.com>
2021-05-02 15:19:27 +0530

    Delete DatabaseConnection.php

--- joke_site_(generic_functions)/includes/DatabaseConnection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=8888;
-                dbname=ijdb;charset=utf8',
-                'root','root');
-$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
